691e4fb60c2899fe7950678dd8362b7e439e9fa8
kdrai007 <987kundanrai@gmail.com>
2023-04-26 18:20:22 +0530

    intital commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+
+
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+
+  return (
+    <main>
+      <h1>hello world</h1>
+    </main>
+  )
+}
+
+export default App
